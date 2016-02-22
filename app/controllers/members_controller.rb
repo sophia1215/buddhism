919cc67f6512373913cb579aff7834531235561e
sophia1215 <code.sophiawang@gmail.com>
2016-02-22 19:37:30 +0800

    Add Member Sign In Before filters

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -24,6 +24,9 @@
 #
 
 class MembersController < ApplicationController
+  before_action :member_signed_in, only: [:edit, :update]
+  before_action :correct_member,   only: [:edit, :update]
+
   def edit
     # All the setup that is needed to build the edit page view
     @member = Member.find(params[:id])
@@ -40,19 +43,16 @@ class MembersController < ApplicationController
     @org_ca[:typ_country_id] = @org_ca[:typ_country_id][:id]
     @org_ca[:typ_region_id] = @org_ca.delete :typ_regions
     @org_ca[:typ_region_id] = @org_ca[:typ_region_id][:id]
-    # @org_ca[:org_company_id] = @org_ca.delete :org_company
-    # @org_ca[:org_company_id] = @org_ca[:org_company_id][:id]
 
     # Edit function variables, in case of failed validations and we re-render :edit
-    @member = Member.find_by(id: current_member.id) #Find the OrgPerson corresponding to org_person_id FK
-    # Using the org_person_id find/create the record and assing the attributes to @contactInfo
+    # Find the Member corresponding to member_id FK
+    @member = Member.find_by(id: current_member.id)
+    # Using the member_id find/create the record and assing the attributes to @contactInfo
     @contactInfo = MemberContact.find_or_initialize_by(member_id: current_member.id).attributes
     # byebug
     @contactInfo[:email] = current_member.email
-    @member.member_contacts.build(@contactInfo) #Build the form using @contactInfo
-    # if !@org_ca[:org_company_id].nil?
-    #   OrgPerson.update(current_org_person.id ,org_company_id: @org_ca[:org_company_id])
-    # end
+    #Build the form using @contactInfo
+    @member.member_contacts.build(@contactInfo)
     # byebug
     # Find contacts record or create them if necessary
     @contact = MemberContact.find_or_create_by(member_id: current_member.id)
@@ -61,8 +61,8 @@ class MembersController < ApplicationController
     if @contact.update_attributes(@org_ca)
       # If this user also controls the information of the company (i.e. COO) then also update company email
       MemberContact.where(email: current_member.email).update_all(email: @org_ca["email"])
-      # If there is a company, update the person record to reflect the company too.
-      flash[:success] = "Profile updated"
+      # If there is a company, update the member record to reflect the company too.
+      flash[:success] = "檔案更新成功！"
       redirect_to edit_member_path(@member.id)
     else # Failed. Re-render the page as unsucessful
       render :edit
@@ -73,5 +73,25 @@ class MembersController < ApplicationController
 
   def update_member_params
     params.require(:member).permit(member_contacts_attributes: [:address1, {typ_countries: :id}, {typ_regions: :id}, :postal_code, :email, :cell_number, :member_id, :avatar])
+  end
+
+  # Before filters
+  def member_signed_in
+    unless signed_in?
+      store_location
+      redirect_to sign_in_url, flash: {warning: "請登入."}
+    end
+  end
+
+  # Checks to see if the current member is actually the member this page is suppose to show
+  # We don't want a person to type in the member of someone else in the link and edit their info
+  def correct_member
+    @member = Member.find_by_id(params[:id])
+    #If member is found, go to edit page, else go to sign in
+    if !@member.nil? && (current_member.id == @member.id)
+    else
+      flash[:danger] = "The request cannot be fulfilled."
+      redirect_to(sign_in_url)
+    end
   end
 end
